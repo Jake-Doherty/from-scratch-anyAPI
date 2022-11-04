8d66b91ccb017d23c68abe1f551ccad4c9fd47d7
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-04 10:40:10 -0700

    add placeholders until I can work on this

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS CHANGETABLENAME;
+
+CREATE TABLE CHANGETABLENAME (
+  change BIGINT GENERATED ALWAYS AS IDENTITY,
+  change VARCHAR NOT NULL,
+  change VARCHAR NOT NULL,
+  change INT NOT NULL,
+  change VARCHAR NOT NULL,
+  change INT NOT NULL,
+  change BOOLEAN NOT NULL
+);
+
+INSERT INTO CHANGETABLENAME (change, change, change, change, change, change)
+VALUES 
