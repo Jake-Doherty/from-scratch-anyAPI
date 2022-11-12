2a7613a6c77f3fd80fcd39e4a1aa4b633ede72f3
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-12 12:22:13 -0800

    add test for /users/:id route

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -9,6 +9,18 @@ describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('/users/:id should show the detail of a user by id', async () => {
+    const resp = await request(app).get('/users/1');
+    const Lannie = {
+      id: '1',
+      first_name: 'Lannie',
+      last_name: 'Sherrott',
+      email: 'lsherrott0@google.com.au',
+      avatar: 'https://robohash.org/utlaboreofficia.png?size=50x50&set=set1',
+      ip_address: '89.235.66.8',
+    };
+    expect(resp.body).toEqual(Lannie);
+  });
   it('/users should return a list of users', async () => {
     const resp = await request(app).get('/users');
     const expected = users.map((user) => {
